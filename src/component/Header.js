fbf085e7a4c934869ef6852216bfba688648bfec
Hyeonkkyu <101560173+Hyeonkkyu@users.noreply.github.com>
2022-09-22 09:11:16 +0900

    22.09.21

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,9 +1,19 @@
 import React from 'react'
+// import { Link } from 'react-router-dom';
+// import NavMenu from './NavMenu';
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+    return (
+        <header className='Header'>
+            <div className='inner'>
+                <h1>
+                    <Link to="/">
+                        <img alt=""/>
+                    </Link>
+                </h1>
+            </div>
+        </header>
+    )
 }
 
 export default Header
