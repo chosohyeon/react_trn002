dbc19de1486b783923792634fea09d807395712d
hyeonkkyu <milktea185ml@naver.com>
2023-01-05 16:44:21 +0900

    23.01.05 부분 수정

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -3,38 +3,45 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='Footer'>
-      <div class="topInfo">
-                <div className="footerMenu">
-                    <dl>
-                        <dd><a href="">About</a></dd>
-                        <dd><a href="">Terms & Conditions</a></dd>
-                        <dd><a href="">Privacy</a></dd>
-                        <dd><a href="">Policy</a></dd>
-                        <dd><a href="">Partners</a></dd>
-                    </dl>
-                </div>
-                <div className="footerInfo">
-                    <address>
-                        <ul>
-                            <li>상호 : 조구만</li>
-                            <li>대표 : 김병준</li>
-                            <li>주소 : 서울시 마포구 월드컵북로5나길 9-6, 2층</li>
-                            <li>사업자등록번호 : 160-81-02444</li>
-                            <li>통신판매업신고증 : 제2022-서울마포-2050호</li>
-                        </ul>
-                        <ul>
-                            <li>E-mail : <a href="mailto:joguman.studio@gmail.com">joguman.studio@gmail.com</a></li>
-                            <li>개인정보관리책임자 : 김병준</li>
-                        </ul>
-                    </address>
-                </div>
-                <div className="copyRight">
-                    <p>Copyright &copy; 2017 Joguman Studio. All Rights Reserved</p>
-                </div>
-            </div>
-            <div className="bottomLogo">
-                <img src="./img/mini_logo_removebg.png" alt=""/>
-            </div>
+      <div className="top">
+        <img src={`${process.env.PUBLIC_URL}/assets/images/logo.png`} alt="" />
+      </div>
+      <div class="bottom">
+        <div className="footerMenu">
+          <dl>
+            <dd><a href="">About</a></dd>
+            <dd><a href="">Terms & Conditions</a></dd>
+            <dd><a href="">Privacy</a></dd>
+            <dd><a href="">Policy</a></dd>
+            <dd><a href="">Partners</a></dd>
+          </dl>
+        </div>
+        <div className="footerInfo">
+          <address className='left'>
+            <ul>
+              <li>Company: (주)왁티</li>
+              <li>Owner: 강정훈</li>
+              <li>06028 서울특별시 강남구 압구정로10길 7 (신사동) 3층 왁티</li>
+            </ul>
+            <ul>
+              <li>Company Registration No.: 240-88-00299</li>
+              <li>Telecommunication services registration No.: 제2016-서울강남-03076호</li>
+            </ul>
+            <ul>
+              <li>E-mail : <a href="support@sw19official.com">support@sw19official.com</a></li>
+              <li>Tel: 070-4457-4194 (Mon-Fri 9:00-11:00 / 13:00-18:00)</li>
+            </ul>
+          </address>
+        </div>
+        <div className="Right">
+          <p>Copyright &copy; Copyright © SW19. All rights reserved.</p>
+          <ul>
+            <li>이용약관</li>
+            <li>개인정보처리방침</li>
+            <li>대량주문 / 제휴문의</li>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
